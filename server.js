def13817af9257b3eeb109ca7f5477349401a4e7
wanching0730 <wanching_97@hotmail.com>
2021-08-04 21:28:46 +0800

    feat: connect to mongodb and create role collections

--- server.js
+++ server.js
@@ -18,6 +18,25 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// connect to MongoDB
+const dbConfig = require ("./config/db.config");
+const db = require("./app/models");
+const Role = db.role;
+
+db.mongoose
+    .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Successfully connected to MongoDB.");
+        initial();
+    })
+    .catch(err => {
+        console.error("Connection error", err);
+        process.exit();
+    });
+
 // simple route
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to ryde tech assessment." });
@@ -28,3 +47,29 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
+
+// initialise types of roles in MongoDB collection
+function initialiseRoles() {
+    Role.estimatedDocumentCount((err, count) => {
+        if (!err && count === 0) {
+            new Role({
+                name: "user"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+
+                console.log("Added 'user' to roles collection");
+            });
+
+            new Role({
+                name: "admin"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+
+                console.log("Added 'admin' to roles collection");
+            });
+        }
+    });

73fa33687e93b4a9d98bbc4cb0e9e2b31a11db51
wanching0730 <wanching_97@hotmail.com>
2021-08-07 20:25:25 +0800

    docs: add documentation for following feature

--- docs/tags.js
+++ docs/tags.js
@@ -3,5 +3,6 @@ module.exports = {
         {name: "User CRUD operations for Normal Users"},
         {name: "Authentication"},
         {name: "User CRUD operations for Admin"},
+        {name: "User's relationship mapping"}
     ],
 };

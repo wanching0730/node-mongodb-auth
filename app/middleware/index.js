8a14985c2e54120abda0934ced713ecb8dbf5860
wanching0730 <wanching_97@hotmail.com>
2021-08-10 22:25:21 +0800

    refactor

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -1,9 +1,7 @@
 const authentication = require("./authenticate");
 const authorization = require("./authorize");
-const verifyRegistration = require("./verifyRegistration");
 
 module.exports = {
     authentication,
-    authorization,
-    verifyRegistration
+    authorization
 };

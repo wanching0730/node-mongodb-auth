dcd138872d43815fbf2a2607a0fadb2c20593781
wanching0730 <wanching_97@hotmail.com>
2021-08-10 23:58:04 +0800

    fix: add async handler for auth

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -5,13 +5,13 @@ const {asyncHandler} = require("../utils/error-handler");
 
 module.exports = function(app) {
     // routes for normal user: Only can select, update, delete own record only
-    app.use("/users", [verifyToken])
+    app.use("/users", [asyncHandler(verifyToken)]);
     app.get("/users", asyncHandler(findOne));
     app.put("/users", asyncHandler(updateOne));
     app.delete("/users", asyncHandler(deleteOne));
 
     // routes for admin: Can select single record, select all records, update, delete any record
-    app.use("/admin/users", [verifyToken, isAdmin])
+    app.use("/admin/users", [asyncHandler(verifyToken), asyncHandler(isAdmin)]);
     app.get("/admin/users/:id", asyncHandler(findOne));
     app.get("/admin/users", asyncHandler(findAll));
     app.put("/admin/users/:id", asyncHandler(updateOne));
